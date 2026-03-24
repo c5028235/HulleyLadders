739158cde0e19b2e8da15c450087cffbd81efcdf
c5028235 <c5028235@hallam.shu.ac.uk>
2026-03-24 07:23:53 +0000

    home template done

--- Accounts/views/homeview.py
+++ Accounts/views/homeview.py
@@ -1,7 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 from Products.models.product import Product
 
 
-class HomePageView(TemplateView):
-    product = Product
+class HomePageView(ListView):
+    model = Product
     template_name= "home.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        return Product.objects.filter(available=True)[:6]
--- Products/models/product.py
+++ Products/models/product.py
@@ -8,7 +8,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.TextField(blank=True, null=True)
     base_price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
-    Image = models.ImageField(upload_to='uploads/products/')
+    image = models.ImageField(upload_to='uploads/products/')
+    available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
