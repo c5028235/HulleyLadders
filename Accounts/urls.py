0809dad944cff9c7c837c270da7090ca46e2b73d
rashy-cloud <rashydahabubakar@gmail.com>
2026-03-25 18:54:00 +0000

    About page

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views.register import RegisterView
 from .views.login import LogInView
 from .views.homeview import HomePageView
+from .views.aboutview import AboutView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('register/', RegisterView.as_view(), name="register"),
     path('login/', LogInView.as_view(), name="login"),
+    path('about/', AboutView.as_view(), name = 'about'),
 ]
