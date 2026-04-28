f215f24e6ec6d33fce66e60477631d3a3aed9de5
Kike35032993 <Kikelomo.L.Ogunruku@student.shu.ac.uk>
2026-04-28 22:56:30 +0100

    Save current work

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -3,10 +3,12 @@ from .views.register import RegisterView
 from .views.login import LogInView
 from .views.homeview import HomePageView
 from .views.aboutview import AboutView
+from .views.logout import LogoutView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LogInView.as_view(), name='login'),
     path('about/', AboutView.as_view(), name = 'about'),
+    path('logout/', LogoutView.as_view(), name = 'logout')
 ]
